120eecc656440fefb54da7a6295264d1ba4629b5
pione30 <g.pionelish30@gmail.com>
2020-08-10 23:45:54 +0900

    establish_connection returns Result

--- src/infra/db_connector.rs
+++ src/infra/db_connector.rs
@@ -2,10 +2,17 @@ use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
+use thiserror::Error;
 
-pub fn establish_connection() -> PgConnection {
+#[derive(Error, Debug)]
+pub enum DbConnectionError {
+    #[error("{0}")]
+    Connection(#[from] diesel::ConnectionError),
+}
+
+pub fn establish_connection() -> Result<PgConnection, DbConnectionError> {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+    PgConnection::establish(&database_url).map_err(DbConnectionError::Connection)
 }
