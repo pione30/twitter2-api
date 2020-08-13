839d47b27c50ff7c627438cbe28634dd8b13dfe3
pione30 <g.pionelish30@gmail.com>
2020-08-13 21:09:21 +0900

    impl custom deserialization behavior for UserId

--- src/domain/model/identity.rs
+++ src/domain/model/identity.rs
@@ -1,3 +1,18 @@
-use diesel::sql_types::Uuid;
+use diesel::Queryable;
+use uuid::Uuid;
 
 pub struct UserId(Uuid);
+
+impl Into<Uuid> for UserId {
+    fn into(self) -> Uuid {
+        self.0
+    }
+}
+
+impl Queryable<diesel::sql_types::Uuid, diesel::pg::Pg> for UserId {
+    type Row = Uuid;
+
+    fn build(row: Self::Row) -> Self {
+        UserId(row)
+    }
+}
