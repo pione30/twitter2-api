31fe626803cd96321a9979b3a29d62df743258fb
pione30 <g.pionelish30@gmail.com>
2020-08-13 21:52:13 +0900

    write some main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 mod infra;
 use infra::establish_connection;
-
+mod domain;
 use anyhow::Result;
+use std::io::stdin;
 
 fn main() -> Result<()> {
     let connection = establish_connection()?;
+
+    println!("Type the name of a new user.");
+    let mut name = String::new();
+    stdin().read_line(&mut name).unwrap();
+    // Drop the newline character
+    let name = &name[..(name.len() - 1)];
+
+    let user = infra::create(&connection, name)?;
+    println!("Saved User with name: {}, id: {}", user.name, user.id);
 
     Ok(())
 }
