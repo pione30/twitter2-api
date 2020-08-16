20bd613086cde7dc85749375b2efa77b0d79e44a
pione30 <g.pionelish30@gmail.com>
2020-08-16 18:45:10 +0900

    specify the deserialization behavior for the identities

--- src/domain/model/post.rs
+++ src/domain/model/post.rs
@@ -1,3 +1,4 @@
+use crate::domain::model::{PostId, UserId};
 use chrono::{DateTime, Utc};
 use diesel::{Insertable, Queryable};
 use twitter2_api::schema::posts;
@@ -5,8 +6,10 @@ use uuid::Uuid;
 
 #[derive(Queryable, Debug, PartialEq, Eq)]
 pub struct Post {
+    #[diesel(deserialize_as = "PostId")]
     pub id: Uuid,
     pub body: String,
+    #[diesel(deserialize_as = "UserId")]
     pub user_id: Uuid,
     pub created_at: DateTime<Utc>,
 }
