b1c09e4493c86eebcba1ae82c0b4f399c1142900
pione30 <g.pionelish30@gmail.com>
2020-10-04 21:31:28 +0900

    add routing for api/v1/posts/own

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
 use crate::app::App;
 use std::future::Future;
 use std::net::SocketAddr;
-use warp::Filter;
+use warp::{filters::BoxedFilter, Filter, Reply};
 
 pub struct Server {
     app: App,
@@ -13,8 +13,36 @@ impl Server {
     }
 
     pub fn run<T: Into<SocketAddr> + 'static>(&self, addr: T) -> impl Future<Output = ()> {
-        let routes = warp::path!("healthcheck").map(|| "ok");
+        let routes = healthcheck().or(api());
 
         warp::serve(routes).run(addr)
     }
+}
+
+fn healthcheck() -> BoxedFilter<(impl Reply,)> {
+    warp::path("healthcheck").map(|| "ok").boxed()
+}
+
+fn api() -> BoxedFilter<(impl Reply,)> {
+    let api = warp::path("api");
+    let v1 = warp::path("v1");
+    let authorization = warp::header::<String>("authorization");
+
+    let posts = warp::path("posts");
+    let own_posts = warp::path("own").map(|| {
+        let post_ids = vec![1, 2, 3, 42];
+        warp::reply::json(&post_ids)
+    });
+    let posts = posts.and(own_posts);
+
+    api.and(
+        v1.and(authorization)
+            .map(|token| {
+                println!("Authorization token: {}", token);
+            })
+            // untuple_one() is necessary
+            .untuple_one()
+            .and(posts),
+    )
+    .boxed()
 }
